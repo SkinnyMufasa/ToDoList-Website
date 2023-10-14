49382582ffa6e0d1e3276ae5b7810f1b6e099cee
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-10-14 14:15:00 +0200

    initail commit

---
+++ src/test/kotlin/dev/kellymahlangu/todo/ToDoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kellymahlangu.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
